bb9f2e8feb2d8042102b441ad431b454a1ce410b
Milton Rodrigues <miltonrodrigues713@gmail.com>
2020-12-04 18:26:07 -0300

    Prettier Code
    Formated with prettier code

--- src/js/script.js
+++ src/js/script.js
@@ -1,44 +1,39 @@
-(function($,sr){
-
+(function ($, sr) {
   // debouncing function from John Hann
   // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
   var debounce = function (func, threshold, execAsap) {
-      var timeout;
+    var timeout;
 
-      return function debounced () {
-          var obj = this, args = arguments;
-          function delayed () {
-              if (!execAsap)
-                  func.apply(obj, args);
-              timeout = null;
-          };
+    return function debounced() {
+      var obj = this,
+        args = arguments;
+      function delayed() {
+        if (!execAsap) func.apply(obj, args);
+        timeout = null;
+      }
 
-          if (timeout)
-              clearTimeout(timeout);
-          else if (execAsap)
-              func.apply(obj, args);
+      if (timeout) clearTimeout(timeout);
+      else if (execAsap) func.apply(obj, args);
 
-          timeout = setTimeout(delayed, threshold || 100);
-      };
-  }
-  // smartresize 
-  jQuery.fn[sr] = function(fn){  return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
-
-})(jQuery,'smartresize');
+      timeout = setTimeout(delayed, threshold || 100);
+    };
+  };
+  // smartresize
+  jQuery.fn[sr] = function (fn) {
+    return fn ? this.bind("resize", debounce(fn)) : this.trigger(sr);
+  };
+})(jQuery, "smartresize");
 
 var $ = jQuery;
 
-
-(function(){
-
-
+(function () {
   ///////////////////////////////
   // Set Home Slideshow Height
   ///////////////////////////////
 
   function setHomeBannerHeight() {
-    var windowHeight = jQuery(window).height(); 
-    jQuery('#header').height(windowHeight);
+    var windowHeight = jQuery(window).height();
+    jQuery("#header").height(windowHeight);
   }
 
   ///////////////////////////////
@@ -46,24 +41,24 @@ var $ = jQuery;
   ///////////////////////////////
 
   function centerHomeBannerText() {
-      var bannerText = jQuery('#header > .center');
-      var bannerTextTop = (jQuery('#header').actual('height')/2) - (jQuery('#header > .center').actual('height')/2) - 40;   
-      bannerText.css('padding-top', bannerTextTop+'px');    
-      bannerText.show();
+    var bannerText = jQuery("#header > .center");
+    var bannerTextTop =
+      jQuery("#header").actual("height") / 2 -
+      jQuery("#header > .center").actual("height") / 2 -
+      40;
+    bannerText.css("padding-top", bannerTextTop + "px");
+    bannerText.show();
   }
 
-  function setHeaderBackground() {    
-    var scrollTop = jQuery(window).scrollTop(); // our current vertical position from the top 
-    
-    if (scrollTop > 300 || jQuery(window).width() < 700) { 
-      jQuery('#header .top').addClass('solid');
+  function setHeaderBackground() {
+    var scrollTop = jQuery(window).scrollTop(); // our current vertical position from the top
+
+    if (scrollTop > 300 || jQuery(window).width() < 700) {
+      jQuery("#header .top").addClass("solid");
     } else {
-      jQuery('#header .top').removeClass('solid');    
+      jQuery("#header .top").removeClass("solid");
     }
   }
-
-
-
 
   ///////////////////////////////
   // Initialize
@@ -74,92 +69,74 @@ var $ = jQuery;
   centerHomeBannerText();
 
   //Resize events
-  jQuery(window).smartresize(function(){
+  jQuery(window).smartresize(function () {
     setHomeBannerHeight();
     centerHomeBannerText();
   });
-  
 })();
 
-
-  ///////////////////////////////
-  // Smooth Scroll
-  ///////////////////////////////
-
+///////////////////////////////
+// Smooth Scroll
+///////////////////////////////
 
 smoothScroll.init();
 
-
-
-
-  ///////////////////////////////
-  // Animate Css
-  ///////////////////////////////
+///////////////////////////////
+// Animate Css
+///////////////////////////////
 var $ = jQuery;
 
-function animationHover(element, animation){
-    element = $(element);
-    element.hover(
-        function() {
-            element.addClass('animated ' + animation);        
-        },
-        function(){
-            //wait for animation to finish before removing classes
-            window.setTimeout( function(){
-                element.removeClass('animated ' + animation);
-            }, 2000);         
-        });
+function animationHover(element, animation) {
+  element = $(element);
+  element.hover(
+    function () {
+      element.addClass("animated " + animation);
+    },
+    function () {
+      //wait for animation to finish before removing classes
+      window.setTimeout(function () {
+        element.removeClass("animated " + animation);
+      }, 2000);
+    }
+  );
 }
 
-$(document).ready(function(){
-    $('#scrollToContent').each(function() {
-        animationHover(this, 'pulse');
-    });
+$(document).ready(function () {
+  $("#scrollToContent").each(function () {
+    animationHover(this, "pulse");
+  });
 });
 
+///////////////////////////////
+// Header Fixed
+///////////////////////////////
 
-
-  ///////////////////////////////
-  // Header Fixed
-  ///////////////////////////////
-
-
-
-var menu = $('#navigation');
+var menu = $("#navigation");
 var origOffsetY = menu.offset().top;
 
 function scroll() {
-   if ($(window).scrollTop() >= origOffsetY) {
-       $('#navigation').addClass('nav-wrap');
-       $('#experiences').addClass('exp');
-       //$('.content').addClass('menu-padding');
-   } else {
-       $('#navigation').removeClass('nav-wrap');
-       $('#experiences').removeClass('exp');
-       //$('.content').removeClass('menu-padding');
-   }
-
-
-
+  if ($(window).scrollTop() >= origOffsetY) {
+    $("#navigation").addClass("nav-wrap");
+    $("#experiences").addClass("exp");
+    //$('.content').addClass('menu-padding');
+  } else {
+    $("#navigation").removeClass("nav-wrap");
+    $("#experiences").removeClass("exp");
+    //$('.content').removeClass('menu-padding');
+  }
 }
 
- document.onscroll = scroll;
+document.onscroll = scroll;
 
+///////////////////////////////
+// Testimonial Slide
+///////////////////////////////
 
-  ///////////////////////////////
-  // Testimonial Slide
-  ///////////////////////////////
-
- $(document).ready(function() {
- 
+$(document).ready(function () {
   $("#testimonial-container").owlCarousel({
- 
-      navigation : false, // Show next and prev buttons
-      slideSpeed : 700,
-      paginationSpeed : 400,
-      singleItem:true,
+    navigation: false, // Show next and prev buttons
+    slideSpeed: 700,
+    paginationSpeed: 400,
+    singleItem: true,
   });
- 
 });
-
-
