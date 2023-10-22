f49a532a2dffe29c4c88458fe584bf6725cdb3dd
Miltonr87 <miltonrodrigues713@gmail.com>
2023-10-22 15:05:34 -0300

    Upgrade visual buttons and text

--- src/js/function.js
+++ src/js/function.js
@@ -1,7 +1,7 @@
-const TypingText = document.querySelector(".typed-text");
-const AutoTyping = document.querySelector(".TypeCursor");
+const TypingText = document.querySelector('.typed-text');
+const AutoTyping = document.querySelector('.TypeCursor');
 
-const textArray = ["Front-End Developer"];
+const textArray = ['FRONT-END DEVELOPER'];
 const typingDelay = 100;
 const erasingDelay = 80;
 const newTextDelay = 1000;
@@ -10,21 +10,21 @@ let charIndex = 0;
 
 function type() {
   if (charIndex < textArray[textArrayIndex].length) {
-    if (!AutoTyping.classList.contains("typing"))
-      AutoTyping.classList.add("typing");
+    if (!AutoTyping.classList.contains('typing'))
+      AutoTyping.classList.add('typing');
     TypingText.textContent += textArray[textArrayIndex].charAt(charIndex);
     charIndex++;
     setTimeout(type, typingDelay);
   } else {
-    AutoTyping.classList.remove("typing");
+    AutoTyping.classList.remove('typing');
     setTimeout(erase, newTextDelay);
   }
 }
 
 function erase() {
   if (charIndex > 0) {
-    if (!AutoTyping.classList.contains("typing"))
-      AutoTyping.classList.add("typing");
+    if (!AutoTyping.classList.contains('typing'))
+      AutoTyping.classList.add('typing');
     TypingText.textContent = textArray[textArrayIndex].substring(
       0,
       charIndex - 1
@@ -32,13 +32,13 @@ function erase() {
     charIndex--;
     setTimeout(erase, erasingDelay);
   } else {
-    AutoTyping.classList.remove("typing");
+    AutoTyping.classList.remove('typing');
     textArrayIndex++;
     if (textArrayIndex >= textArray.length) textArrayIndex = 0;
     setTimeout(type, typingDelay + 1100);
   }
 }
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', function () {
   if (textArray.length) setTimeout(type, newTextDelay + 250);
 });
