f5e6a8d6eed0db652890da8c44bd15004b125a24
Miltonr87 <miltonrodrigues713@gmail.com>
2023-10-22 17:48:32 -0300

    Added new border

--- src/js/function.js
+++ src/js/function.js
@@ -1,7 +1,7 @@
 const TypingText = document.querySelector('.typed-text');
 const AutoTyping = document.querySelector('.TypeCursor');
 
-const textArray = ['FRONT-END DEVELOPER'];
+const textArray = ['Front-End Developer'];
 const typingDelay = 100;
 const erasingDelay = 80;
 const newTextDelay = 1000;
